7b2b23be29b877802e58d6f59fd10df1a6a261c8
mohsvr <33391654+mohsvr@users.noreply.github.com>
2018-06-05 10:55:34 +0100

    3.0

--- Lorann/main/src/main/java/main/Main.java
+++ Lorann/main/src/main/java/main/Main.java
@@ -1,33 +1,26 @@
 package main;
 
-import java.sql.SQLException;
-
-import controller.ControllerFacade;
-import model.ModelFacade;
-import view.ViewFacade;
+import controller.Controller;
+import model.Model;
+import view.View;
 
 /**
- * <h1>The Class Main.</h1>
- *
- * @author Jean-Aymeric DIET jadiet@cesi.fr
- * @version 1.0
+ * The Class Main.
+ * @author Jean-Aymeric Diet
  */
 public abstract class Main {
 
-    /**
-     * The main method.
-     *
-     * @param args
-     *            the arguments
-     */
-    public static void main(final String[] args) {
-        final ControllerFacade controller = new ControllerFacade(new ViewFacade(), new ModelFacade());
-
-        try {
-            controller.start();
-        } catch (final SQLException exception) {
-            exception.printStackTrace();
-        }
-    }
-
-}
+	/**
+	 * The main method.
+	 *
+	 * @param args
+	 *          the arguments
+	 */
+	public static void main(final String[] args) {
+		final Model			model = new Model();
+		final View			view = new View(model);
+		final Controller	controller = new Controller(view, model);
+		view.setController(controller);
+		controller.start();
+	}
+} 
