ee781e5cbc936ae6c083cafbd3fa96fd689864c7
mohsvr <33391654+mohsvr@users.noreply.github.com>
2018-06-05 12:15:00 +0100

    fifth

---
+++ Lorann/view/src/test/java/view/ViewTest.java
@@ -0,0 +1,74 @@
+package view;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ViewTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testView() {
+		
+	}
+
+	@Test
+	public void testGetHeight() {
+		
+	}
+
+	@Test
+	public void testGetWidth() {
+		
+	}
+
+	@Test
+	public void testKeyCodeToControllerOrder() {
+		
+	}
+
+	@Test
+	public void testPrintMessage() {
+		
+	}
+
+	@Test
+	public void testGetPseudo() {
+		
+	}
+
+	@Test
+	public void testRun() {
+		
+	}
+
+	@Test
+	public void testRepaint() {
+		
+	}
+
+	@Test
+	public void testSetController() {
+		
+	}
+
+}
